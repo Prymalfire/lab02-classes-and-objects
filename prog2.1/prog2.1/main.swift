3dfb2eb1bacdbfcfcc16961b4593bc5d5908461e
James Strathern <jstrathern@oucs1423.otago.ac.nz>
2018-07-18 12:16:29 +1200

    End of Lab 02

--- prog2.1/prog2.1/main.swift
+++ prog2.1/prog2.1/main.swift
@@ -1,20 +1,16 @@
-//
-//  main.swift
-//  prog2.1
-//
-//  Created by James Strathern on 7/18/18.
-//  Copyright © 2018 James Strathern. All rights reserved.
-//
-
 import Foundation
 
+// Create and initialise a new Fraction object
 var u: Fraction
 
-u = Fraction(num: 2, den: 3)
+// Initialise the object
+u = Fraction(num: 2,den: 3)
 
+// Show the state of the object's internal variables
 print("u=\(u.description)")
 print("decimal value of u=\(u.decimal)")
 
+// Define and create a new instance of a Fraction object
 var x: Fraction = Fraction(num: -5)
 
 print("x=\(x.description)")
@@ -27,14 +23,21 @@ print("decimal value of v=\(v.decimal)")
 
 var f: Fraction
 
-f = Fraction.add(u, to: v)
+f = u+v
 print("(" + u.description + ")+(" + v.description + ")=" + f.description)
 
-f = Fraction.subtract(u, from: v)
+f = v-u
 print("(" + u.description + ")-(" + v.description + ")=" + f.description)
 
-f = Fraction.multiply(u, by: v)
+f = u*v
 print("(" + u.description + ")*(" + v.description + ")=" + f.description)
 
-f = Fraction.divide(u, by: v)
+f = u/v
 print("(" + u.description + ")/(" + v.description + ")=" + f.description)
+
+f = u.add(3)
+print("(" + u.description + ")+3=" + f.description)
+
+let z: Int = 2
+f = u.divide(z)
+print("(" + u.description + ")/\(z)=" + f.description)
